8727591bd6b32ca83fba74ad4d7aa7928e3b8d27
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-05-21 16:09:33 +0200

    Act 1

---
+++ BuildWin.bat
@@ -0,0 +1,4 @@
+Clear.bat
+python -m PyInstaller --onefile --windowed --noconsole --icon=backrooms-escape-together.ico NoCompatibleToInstall.py
+python -m PyInstaller --onefile --windowed --noconsole --icon=backrooms-escape-together.ico PlayGame.py
+echo v0.13.3 > GameVersion.txt
