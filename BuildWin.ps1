977eda0079a490d0b9b0fb2e505fa21c672ccc37
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-06-11 18:50:06 +0200

    Act 2

--- BuildWin.ps1
+++ BuildWin.ps1
@@ -1,4 +1,4 @@
 ./Clear.ps1
 python -m PyInstaller --onefile --windowed --noconsole --icon=backrooms-escape-together.ico NoCompatibleToInstall.py
 python -m PyInstaller --onefile --windowed --noconsole --icon=backrooms-escape-together.ico PlayGame.py
-echo v0.13.3 > GameVersion.txt
+echo v0.14.6.1-15241 > GameVersion.txt
